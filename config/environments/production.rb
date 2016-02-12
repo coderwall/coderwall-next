456a4380f15d392ef77b0aa3db55e1fb66f39501
mdeiters <mdeiters@gmail.com>
2016-02-11 21:10:52 -0800

    forcing ssl in production, testing auto-deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'coderwall.com' }
+  config.action_mailer.default_url_options = { host: 'next.coderwall.com' }
+  config.force_ssl = true
 end
