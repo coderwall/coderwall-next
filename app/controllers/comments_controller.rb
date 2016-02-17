492464a0b1237bcd893f2b578cebe4159691b525
mdeiters <mdeiters@gmail.com>
2016-02-17 15:55:23 -0800

    wired up comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  before_action :require_login, only: [:create, :update]
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    if !@comment.save
+      flash[:error] = "Your comment did not save. #{@comment.errors.full_messages.join(' ')}"
+      flash[:data] = @comment.body
+      redirect_to request.referer + '#new-comment'
+    else
+      redirect_to request.referer + "##{@comment.dom_id}"
+    end
+  end
+
+  protected
+  def comment_params
+    params.require(:comment).permit(:body, :protip_id)
+  end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,13 @@ class Comment < ActiveRecord::Base
   belongs_to :protip
   has_many :likes, as: :likable, dependent: :destroy
 
-  validates :body, length: { minimum: 2 }
+  validates :protip, presence: true
+  validates :user,   presence: true
+  validates :body,   length: { minimum: 2 }
 
   scope :recently_created, ->(count=10) { order(created_at: :desc).limit(count)}
+
+  def dom_id
+    ActionView::RecordIdentifier.dom_id(self)
+  end
 end
