4bc71c824917a80fd16da3894f009ed0c6ccb150
mdeiters <mdeiters@gmail.com>
2016-02-17 16:05:39 -0800

    enabled deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :require_login, only: [:create, :update]
+  before_action :require_login, only: [:create, :destroy]
 
   def create
     @comment = Comment.new(comment_params)
@@ -7,15 +7,29 @@ class CommentsController < ApplicationController
     if !@comment.save
       flash[:error] = "Your comment did not save. #{@comment.errors.full_messages.join(' ')}"
       flash[:data] = @comment.body
-      redirect_to request.referer + '#new-comment'
+      redirect_to_protip_comment_form
     else
-      redirect_to request.referer + "##{@comment.dom_id}"
+      redirect_to_protip_comment(@comment)
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    return head(:forbidden) unless current_user.can_edit?(@comment)
+    @comment.destroy
+    redirect_to_protip_comment_form
+  end
+
   protected
+  def redirect_to_protip_comment(comment)
+    redirect_to "#{request.referer}##{comment.dom_id}"
+  end
+
+  def redirect_to_protip_comment_form    
+    redirect_to "#{request.referer}#new-comment"
+  end
+
   def comment_params
     params.require(:comment).permit(:body, :protip_id)
   end
-
 end
