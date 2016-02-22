5be3b79187437b663c2573757b886da3d418faf1
mdeiters <mdeiters@gmail.com>
2016-02-22 15:06:10 -0800

    fixed issue with cookie domain

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,6 @@
 # https://github.com/thoughtbot/clearance
 Clearance.configure do |config|
-  config.cookie_domain = '.coderall.com'
+  config.cookie_domain = '.coderwall.com'
   config.httponly = false
   config.routes = false
   config.mailer_sender = "support@coderwall.com"
