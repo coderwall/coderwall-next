9e74db4b2fd38c18001a157a4d7f7cb2836784f6
mdeiters <mdeiters@gmail.com>
2016-02-22 20:20:26 -0800

    trying to get caching working

--- config/initializers/memcache.rb
+++
@@ -1,7 +0,0 @@
-if ENV["MEMCACHEDCLOUD_SERVERS"]
-  Rails.application.config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), {
-    username:  ENV["MEMCACHEDCLOUD_USERNAME"],
-    password:  ENV["MEMCACHEDCLOUD_PASSWORD"],
-    pool_size: Integer(ENV["MEMCACHEDCLOUD_POOL"] || 5)
-  }
-end
