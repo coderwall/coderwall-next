d78f7da8934729cee3ba32670f23b432b3fc4efa
mdeiters <mdeiters@gmail.com>
2016-02-22 18:21:53 -0800

    suppress exceptions for unexpected request types

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   def show
     sanitized_params = params.permit(:page, :layout)
-    render action: sanitized_params[:page]
+    respond_to do |format|
+      format.html { render(action: sanitized_params[:page]) }
+      format.all  { head(:not_found) }
+    end
   end
 end
