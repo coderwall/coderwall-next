b48891396a5a3a378d6cfa2d5831f04a66e7c6e2
Dave Newman <ddangerous@gmail.com>
2016-11-03 12:47:20 +0700

    Ignore crawl js from lint

--- client/checkLinks.js
+++ client/checkLinks.js
@@ -1,29 +1,30 @@
 #!/usr/bin/env node
-var roboto = require('roboto');
+/* eslint-disable */
+const roboto = require('roboto')
 
-var crawler = new roboto.Crawler({
+const crawler = new roboto.Crawler({
   startUrls: [
-    'http://coderwall.dev:5000/'
+    'http://coderwall.dev:5000/',
   ],
   // We don't want it crawling outside links.
   constrainToRootDomains: true,
-});
+})
 
-var deadLinks = [];
-crawler.on('httpError', function(statusCode, href, referer) {
+const deadLinks = []
+crawler.on('httpError', (statusCode, href, referer) => {
   if (statusCode === 404) {
     deadLinks.push({
-      href: href,
-      referer: referer
-    });
+      href,
+      referer,
+    })
   }
-});
+})
 
-crawler.on('finish', function() {
-  for (var i = 0; i < deadLinks.length; i ++) {
-    var deadLink = deadLinks[i];
-    console.log('Dead link: %s  found on page: %s', deadLink.href, deadLink.referer);
+crawler.on('finish', () => {
+  for (let i = 0; i < deadLinks.length; i++) {
+    const deadLink = deadLinks[i]
+    console.log('Dead link: %s  found on page: %s', deadLink.href, deadLink.referer)
   }
-});
+})
 
-crawler.crawl();
+crawler.crawl()
