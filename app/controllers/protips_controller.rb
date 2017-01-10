692e98514334a1183448880b8acb4b7978c3a149
Dave Newman <ddangerous@gmail.com>
2017-01-10 12:53:15 -0800

    also flag spam so it doesn't show up

--- app/controllers/protips_controller.rb
+++ app/controllers/protips_controller.rb
@@ -44,7 +44,7 @@ class ProtipsController < ApplicationController
   def mark_spam
     @protip = Protip.find_by_public_id!(params[:protip_id])
     @protip.spam!
-    @protip.touch(:spam_detected_at)
+    @protip.update!(spam_detected_at: Time.now, flagged: true)
     flash[:notice] = "Marked as spam"
     redirect_to slug_protips_url(id: @protip.public_id, slug: @protip.slug)
   end
